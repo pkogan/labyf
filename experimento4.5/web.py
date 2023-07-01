dbb09a4a57bde5b86f9458d48be41377631527f8
pkogan <pablo.kogan@fi.uncoma.edu.ar>
2023-07-01 07:06:37 -0300

    webkit

---
+++ experimento4.5/web.py
@@ -0,0 +1,14 @@
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+if __name__ == "__main__":
+    app = QApplication([])
+    view = QWebEngineView()
+    #view.load(QUrl("https://www.example.com"))
+    #view.load(QUrl.fromLocalFile("/home/pkogan/Proyectos/labyf/experimento4.5/testshuffle.html"))
+    view.load(QUrl.fromLocalFile("/home/pkogan/Proyectos/labyf/experimento4.5/testshuffle_2_parawebkit.html"))
+    #view.load(QUrl.fromLocalFile("/home/pkogan/Proyectos/labyf/experimento4.5/index.html"))
+
+    view.show()
+    app.exec()
